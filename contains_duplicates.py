e64f6b17b00cd76915db303f61ab03edc08c0fbf
lanbow93 <the.doctor1501@gmail.com>
2023-09-26 08:31:15 -0500

    contains duplicate python pre-video attempt

---
+++ contains_duplicates.py
@@ -0,0 +1,7 @@
+# Solution attempt after watching javascript video before python video
+# Results: Runtime 483ms - Beat 38.62% | Memory 30.78 - Beat 88.56%
+def containsDuplicate(nums):
+    return len(set(nums)) != len(nums)
+
+
+
