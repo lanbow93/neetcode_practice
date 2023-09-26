0cb6e1d6c9b6a9a9dd0a7991dccf6afd95b86380
lanbow93 <the.doctor1501@gmail.com>
2023-09-26 09:50:33 -0500

    contains duplicate python post-video attempt

--- contains_duplicates.py
+++ contains_duplicates.py
@@ -3,5 +3,14 @@
 def containsDuplicate(nums):
     return len(set(nums)) != len(nums)
 
+# Solution attempt after watching video
+# Results: Runtime 477ms - Beat 54.54% | Memory 31.78 - Beat 38.45%
+def postContainsDuplicate(nums):
+    numsHashset = set()
+    for n in nums:
+        if n in numsHashset:
+            return True
+        numsHashset.add(n)
+    return False
 
 
