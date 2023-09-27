5c236796ff97628b883eb2afecdea6534e688e2e
lanbow93 <the.doctor1501@gmail.com>
2023-09-27 09:41:50 -0500

    two sums solution post-video

--- two_sums.py
+++ two_sums.py
@@ -10,6 +10,16 @@ def two_sums(nums, target):
                 return [i, nums.index(target-nums[i] , i+1)]
             except ValueError:
                 continue
-    
-print(two_sums([2,7,11,15], 9))
-print(two_sums([3,2,4, 6],6))
+
+# Solution attempt after watching video
+# Results: Runtime 54ms - Beat 96.21% | Memory 17.65 MB - Beat 29.28%
+def post_two_sums(nums, target):
+    numDict = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in numDict:
+            return [i, numDict[complement]]
+        numDict[num] = i
+
+print(post_two_sums([2,7,11,15], 9))
+print(post_two_sums([3,2,4, 6],6))
