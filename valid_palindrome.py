ed2ea6add382a08d86da20abeab2555410871e7e
lanbow93 <the.doctor1501@gmail.com>
2023-10-13 06:50:41 -0500

    post video solution

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -17,4 +17,43 @@ def isPalindrome(s):
             strippedArray.append("")
     return "".join(reversedArray) == "".join(strippedArray)
 
-isPalindrome(test1)
+# Solution attempt after watching video 
+# Results: Runtime 75ms - Beat 6.40% | Memory 17.04 - Beat 76.19%
+def postIsPanindrome(s):
+    l_pointer = 0
+    r_pointer = len(s) -1
+    
+    def isAlphaNumeric(char):
+        return ('a' <= char <= 'z') or ('A' <= char <= 'Z') or ('0' <= char <= '9')
+    while(l_pointer < r_pointer):
+        while (not isAlphaNumeric(s[l_pointer]) and l_pointer < r_pointer):
+            l_pointer += 1
+        while (not isAlphaNumeric(s[r_pointer]) and l_pointer < r_pointer):
+            r_pointer -= 1
+        if(s[l_pointer].lower() != s[r_pointer].lower()):
+            return False
+        l_pointer += 1
+        r_pointer -= 1
+        
+    return True
+print(postIsPanindrome(test1))
+# Altered Solution Attempt
+# Results: Runtime 66ms - Beat 12.17% | Memory 17.04 - Beat 76.19%
+def isPalindrome(s):
+    l_pointer = 0
+    r_pointer = len(s) - 1
+
+    while l_pointer < r_pointer:
+        while l_pointer < r_pointer and not ('a' <= s[l_pointer] <= 'z' or 'A' <= s[l_pointer] <= 'Z' or '0' <= s[l_pointer] <= '9'):
+            l_pointer += 1
+
+        while l_pointer < r_pointer and not ('a' <= s[r_pointer] <= 'z' or 'A' <= s[r_pointer] <= 'Z' or '0' <= s[r_pointer] <= '9'):
+            r_pointer -= 1
+
+        if s[l_pointer].lower() != s[r_pointer].lower():
+            return False
+
+        l_pointer += 1
+        r_pointer -= 1
+
+    return True
